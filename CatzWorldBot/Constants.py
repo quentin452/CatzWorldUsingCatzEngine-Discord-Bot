34074cf3737a3bb57143e93e19f099724a3d91f1
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-07-14 08:59:55 +0200

    fix a mistake

--- CatzWorldBot/Constants.py
+++ CatzWorldBot/Constants.py
@@ -8,7 +8,7 @@ class ConstantsClass:
     ITCH_IO_GAME_API_KEY = "itch_io_api_key"
     ITCH_IO_GAME_ID = "itch_io_game_id"
     DISCORD_BOT_TOKEN = "discord_bot_token"
-    ROLE_NAME = "@Bot"
+    ROLE_NAME = "CatWorld game ping updates"
     URL_GAME_NAME = "catzworld"
     RSS_URL = f"https://iamacatfrdev.itch.io/{URL_GAME_NAME}/devlog.rss"
     RSS_CHANNEL_IDS_JSON_FILE ="/rss_channel_ids.json"
