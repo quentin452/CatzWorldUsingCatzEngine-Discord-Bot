395eaed721f6afe00a0201e5fa2baec93b07553b
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-07-16 22:02:55 +0200

    Fixed alot of errors for /help_cat command

--- CatzWorldBot/commands/help_commands.py
+++ CatzWorldBot/commands/help_commands.py
@@ -1,76 +1,60 @@
-import discord
 from discord.ext import commands
+from utils.async_logs import LogMessageAsync
+import discord
 
-class CustomHelpCommand(commands.MinimalHelpCommand):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+class CustomHelpCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-    async def send_pages(self):
-        destination = self.get_destination()
-        for page in self.paginator.pages:
-            await destination.send(page)
+    @commands.command(help="command that provide a list of all commands from the bot and documented.")
+    async def help_cat(self, ctx):
+        embed = discord.Embed(title="Aide par catégorie", description="Voici les catégories d'aide disponibles :", color=discord.Color.blue())
+        for cog_name, cog in self.bot.cogs.items():
+            command_list = "\n".join([f"{self.bot.command_prefix}{command.name}: {command.short_doc or 'Pas de description'}" for command in cog.get_commands() if not command.hidden])
+            if command_list:
+                embed.add_field(name=cog_name, value=command_list, inline=False)
+        
+        await ctx.send(embed=embed)
 
     async def send_bot_help(self, mapping):
-        embed = discord.Embed(
-            title="Bot Commands",
-            description="Here is a list of all commands you can use:",
-            color=discord.Color.blue()
-        )
-
+        embed = discord.Embed(title="Commandes du bot", color=discord.Color.blue())
         for cog, commands in mapping.items():
             if cog:
                 cog_name = cog.qualified_name
             else:
-                cog_name = "Uncategorized"
+                cog_name = "Non catégorisé"
 
-            command_list = "\n".join([f"{command.name}: {command.help or 'No description provided'}" for command in commands if not command.hidden])
+            command_list = "\n".join([f"{self.bot.command_prefix}{command.name}: {command.help or 'Pas de description'}" for command in commands if not command.hidden])
             if command_list:
                 embed.add_field(name=cog_name, value=command_list, inline=False)
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            embed = discord.Embed(title="Aide du bot", description=page, color=discord.Color.blue())
+            await destination.send(embed=embed)
 
-        await self.get_destination().send(embed=embed)
 
     async def send_command_help(self, command):
-        if command.hidden or any(isinstance(check, commands.has_permissions) and check.kwargs.get('administrator', False) for check in command.checks):
+        if command.hidden or any(check.__class__.__name__ == 'has_permissions' and check.kwargs.get('administrator', False) for check in command.checks):
             return
+        embed = discord.Embed(title=f"Aide pour {self.bot.command_prefix}{command.name}", color=discord.Color.blue())
+        embed.add_field(name="Description", value=command.help or "Pas de description", inline=False)
+        embed.add_field(name="Utilisation", value=self.get_command_signature(command), inline=False)
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            embed = discord.Embed(title="Aide du bot", description=page, color=discord.Color.blue())
+            await destination.send(embed=embed)
 
-        embed = discord.Embed(
-            title=f"Help with {command.name}",
-            description=command.help or "No description provided",
-            color=discord.Color.blue()
-        )
-        embed.add_field(name="Usage", value=self.get_command_signature(command), inline=False)
-        await self.get_destination().send(embed=embed)
 
     async def send_cog_help(self, cog):
-        embed = discord.Embed(
-            title=f"{cog.qualified_name} Commands",
-            description=cog.description or "No description provided",
-            color=discord.Color.blue()
-        )
-
-        command_list = "\n".join([f"{command.name}: {command.help or 'No description provided'}" for command in cog.get_commands() if not command.hidden])
+        embed = discord.Embed(title=f"Commandes de {cog.qualified_name}", color=discord.Color.blue())
+        command_list = "\n".join([f"{self.bot.command_prefix}{command.name}: {command.help or 'Pas de description'}" for command in cog.get_commands() if not command.hidden])
         if command_list:
-            embed.add_field(name="Commands", value=command_list, inline=False)
+            embed.add_field(name="Commandes", value=command_list, inline=False)
+        destination = self.get_destination()
+        for page in self.paginator.pages:
+            embed = discord.Embed(title="Aide du bot", description=page, color=discord.Color.blue())
+            await destination.send(embed=embed)
 
-        await self.get_destination().send(embed=embed)
-
-    async def send_group_help(self, group):
-        if group.hidden or any(isinstance(check, commands.has_permissions) and check.kwargs.get('administrator', False) for check in group.checks):
-            return
-
-        embed = discord.Embed(
-            title=f"Help with {group.name}",
-            description=group.help or "No description provided",
-            color=discord.Color.blue()
-        )
-        embed.add_field(name="Usage", value=self.get_command_signature(group), inline=False)
-
-        if group.commands:
-            command_list = "\n".join([f"{command.name}: {command.help or 'No description provided'}" for command in group.commands if not command.hidden])
-            if command_list:
-                embed.add_field(name="Subcommands", value=command_list, inline=False)
-
-        await self.get_destination().send(embed=embed)
 
 async def setup(bot):
-    bot.help_command = CustomHelpCommand()
+    await bot.add_cog(CustomHelpCommand(bot))
