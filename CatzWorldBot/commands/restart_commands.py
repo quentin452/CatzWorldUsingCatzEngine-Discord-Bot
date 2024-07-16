36e9b2f11150afdbc92b4b46d0663e058610d441
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-07-16 22:14:19 +0200

    Probably fix issues of restart command

--- CatzWorldBot/commands/restart_commands.py
+++ CatzWorldBot/commands/restart_commands.py
@@ -4,27 +4,28 @@ import os
 import discord
 from discord.ext import commands
 
-is_restarting = False 
-
 class RestartCommand(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.is_restarting = False
 
     @commands.command()
-    @commands.has_permissions(administrator=True) 
+    @commands.has_permissions(administrator=True)
     async def restart(self, ctx):
-        global is_restarting 
-        if not is_restarting:
-            is_restarting = True
+        if not self.is_restarting:
+            self.is_restarting = True
             await ctx.send("Redémarrage du bot...")
             await self._restart_bot()
 
     async def _restart_bot(self):
         await self.bot.close()
-        
-        # Redémarrage du script Python
+
+        # Restart using subprocess
         python = sys.executable
-        os.execl(python, python, *sys.argv)
+        subprocess.Popen([python, *sys.argv])
+
+        # Exit the current process
+        sys.exit()
 
 async def setup(bot):
     await bot.add_cog(RestartCommand(bot))
