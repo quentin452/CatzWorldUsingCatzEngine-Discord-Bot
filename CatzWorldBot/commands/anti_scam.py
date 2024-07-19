8def6134b067ea7d54d4b923791150b6e087d08d
quentin452 <42176772+quentin452@users.noreply.github.com>
2024-07-19 12:42:56 +0200

    Update anti scam

--- CatzWorldBot/commands/anti_scam.py
+++ CatzWorldBot/commands/anti_scam.py
@@ -10,8 +10,11 @@ phishing_keywords = ["free", "win", "giveaway", "click here", "visit", "congratu
 # Regex pour détecter les URL
 phishing_regex = re.compile(r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+')
 
-# Regex pour les URL YouTube et Google à exclure
-whitelist_regex = re.compile(r'https?://(www\.)?(youtube\.com|youtu\.be|google\.com|docs\.google\.com|drive\.google\.com|imgur\.com|printscreen\.com|prntscr\.com|flickr\.com)')
+# Regex pour les URL suspectes, incluant Discord avec différentes variantes de protocoles et sans protocole
+suspicious_regex = re.compile(r'(https?://|http://|www\.)?(discord\.gg|discord\.com)')
+
+# Regex pour les URL à exclure des vérifications (YouTube, Google, etc.)
+whitelist_regex = re.compile(r'https?://(?:www\.)?(youtube\.com|youtu\.be|google\.com|docs\.google\.com|drive\.google\.com|imgur\.com|printscreen\.com|prntscr\.com|flickr\.com)')
 
 class AntiScam(commands.Cog):
     def __init__(self, bot):
@@ -23,19 +26,19 @@ class AntiScam(commands.Cog):
             return
 
         # Vérifiez les messages de l'utilisateur cible
-       # if message.author.id == ConstantsClass.IAMACAT_USER_ID:
-            # Vérification des mots-clés
+        # if message.author.id == ConstantsClass.IAMACAT_USER_ID:
+        # Vérification des mots-clés
         if any(keyword in message.content.lower() for keyword in phishing_keywords):
             try:
-                    # Supprime le message
+                # Supprime le message
                 await message.delete()
-                    
-                    # Envoie un message en MP à l'utilisateur
+
+                # Envoie un message en MP à l'utilisateur
                 user = message.author
                 try:
                     await user.send("Votre message a été supprimé car il pourrait contenir du contenu de phishing ou de scam.")
                 except discord.Forbidden:
-                        # Le bot n'a pas la permission d'envoyer des MP à cet utilisateur
+                    # Le bot n'a pas la permission d'envoyer des MP à cet utilisateur
                     await LogMessageAsync.LogAsync("Le bot n'a pas les permissions nécessaires pour envoyer des messages privés à l'utilisateur.")
                     
             except discord.Forbidden:
@@ -46,23 +49,23 @@ class AntiScam(commands.Cog):
             # Vérification des liens URL
         elif phishing_regex.search(message.content) and not whitelist_regex.search(message.content):
             try:
-                    # Supprime le message
+                # Supprime le message
                 await message.delete()
-                    
-                    # Envoie un message en MP à l'utilisateur
+
+                # Envoie un message en MP à l'utilisateur
                 user = message.author
                 try:
                     await user.send("Votre message a été supprimé car il pourrait contenir du contenu de phishing ou de scam.")
                 except discord.Forbidden:
-                        # Le bot n'a pas la permission d'envoyer des MP à cet utilisateur
+                    # Le bot n'a pas la permission d'envoyer des MP à cet utilisateur
                     await LogMessageAsync.LogAsync("Le bot n'a pas les permissions nécessaires pour envoyer des messages privés à l'utilisateur.")
                     
-                    # Optionnel : Envoie également un message dans le canal où le message a été supprimé
+                # Optionnel : Envoie également un message dans le canal où le message a été supprimé
                 if isinstance(message.channel, discord.DMChannel):
-                        # Message en MP
+                    # Message en MP
                     await message.channel.send(f'{message.author.mention}, votre message a été supprimé car il pourrait contenir du contenu de phishing ou de scam.')
                 else:
-                        # Message dans le canal
+                    # Message dans le canal
                     await message.channel.send(f'{message.author.mention}, votre message a été supprimé car il pourrait contenir du contenu de phishing ou de scam.')
                     
             except discord.Forbidden:
@@ -70,11 +73,38 @@ class AntiScam(commands.Cog):
             except discord.HTTPException as e:
                 await LogMessageAsync.LogAsync(f"Une erreur est survenue en essayant de supprimer le message: {e}")
 
-            # Note: ne pas oublier de faire passer les commandes
+        # Vérification spécifique des liens Discord (à marquer comme suspects)
+        elif suspicious_regex.search(message.content):
+            try:
+                # Marquer le message comme suspect
+                # (Vous pouvez ajouter un rôle, envoyer un message d'alerte ou tout autre traitement ici)
+                await message.delete()
+                # Envoie un message en MP à l'utilisateur
+                user = message.author
+                try:
+                    await user.send("Votre message contient un lien Discord, ce qui peut être suspect. Veuillez vérifier le contenu.")
+                except discord.Forbidden:
+                    # Le bot n'a pas la permission d'envoyer des MP à cet utilisateur
+                    await LogMessageAsync.LogAsync("Le bot n'a pas les permissions nécessaires pour envoyer des messages privés à l'utilisateur.")
+                    
+                # Optionnel : Envoie également un message dans le canal où le message a été marqué comme suspect
+                if isinstance(message.channel, discord.DMChannel):
+                    # Message en MP
+                    await message.channel.send(f'{message.author.mention}, votre message contient un lien Discord, ce qui peut être suspect.')
+                else:
+                    # Message dans le canal
+                    await message.channel.send(f'{message.author.mention}, votre message contient un lien Discord, ce qui peut être suspect.')
+                    
+            except discord.Forbidden:
+                await LogMessageAsync.LogAsync("Le bot n'a pas les permissions nécessaires pour envoyer des messages.")
+            except discord.HTTPException as e:
+                await LogMessageAsync.LogAsync(f"Une erreur est survenue en essayant de traiter le message: {e}")
+
+        # Note: ne pas oublier de faire passer les commandes
         await self.bot.process_commands(message)
-       # else:
-            # Si le message provient d'un autre utilisateur, continuez à traiter les autres événements/messages
-        #    await self.bot.process_commands(message)
+        # else:
+        # Si le message provient d'un autre utilisateur, continuez à traiter les autres événements/messages
+        # await self.bot.process_commands(message)
 
 async def setup(bot):
     await bot.add_cog(AntiScam(bot))
--- CatzWorldBot/commands/discord_loggers/on_message_logs.py
+++ CatzWorldBot/commands/discord_loggers/on_message_logs.py
@@ -55,76 +55,7 @@ class OnMessageLogs(commands.Cog):
                 await log_channel.send(f"Error logging message edit: {e}")
 
     
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        invite_pattern = re.compile(r'(https?:\/\/)?(www\.)?discord\.gg\/\w+')
-        # Ne pas loguer les messages envoyés par le bot lui-même
-        if message.author == self.bot.user:
-            return
-        
-        # Vérifie si le message contient un lien d'invitation Discord
-        if invite_pattern.search(message.content):
-            log_channel = self.bot.get_channel(self.log_channel_id)
-            if log_channel:
-                try:
-                    embed = discord.Embed(title="Invitation Detected",
-                                          description=f"Message contenant une invitation détecté de {message.author.mention}",
-                                          color=discord.Color.red())
-                    embed.add_field(name="Message", value=message.content)
-                    embed.add_field(name="Channel", value=message.channel.mention)
-                    embed.set_footer(text=f"Message ID: {message.id}")
-                    await log_channel.send(embed=embed)
-                except Exception as e:
-                    # Gestion des erreurs lors de l'envoi du message de log
-                    await log_channel.send(f"Error logging invitation: {e}")
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        # Ignore messages sent by the bot itself
-        if message.author == self.bot.user:
-            return
-
-        # Check if the message contains a specific Discord invitation link
-        if re.compile(r'https?:\/\/(?:www\.)?(?:discord\.gg\/\w+|discord\.com\/invite\/\w+)|(?:www\.)?(?:discord\.gg\/\w+|discord\.com\/invite\/\w+)').search(message.content):
-            log_channel = self.bot.get_channel(self.log_channel_id)
-            if log_channel:
-                try:
-                    # Create the embed for logging
-                    embed = discord.Embed(
-                        title="Invitation Detected",
-                        description=f"Message containing an invitation detected from {message.author.mention}",
-                        color=discord.Color.red()
-                    )
-                    embed.add_field(name="Message", value=message.content)
-                    # Check if the message is from a guild or a DM
-                    if message.guild:
-                        embed.add_field(name="Channel", value=message.channel.mention)
-                    else:
-                        embed.add_field(name="Channel", value="Direct Message")
-                    embed.set_footer(text=f"Message ID: {message.id}")
-
-                    # Send the embed to the log channel
-                    await log_channel.send(embed=embed)
-                except Exception as e:
-                    # Handle errors during logging
-                    await log_channel.send(f"Error logging invitation: {e}")
-
-            try:
-                # Delete the message containing the invitation
-                await message.delete()
-
-                # Create and send the warning embed
-                warning_embed = discord.Embed(
-                    title="Warning",
-                    description=f"{message.author.mention}, links to external servers are not allowed. Please review the rules.",
-                    color=discord.Color.orange()
-                )
-
-                await message.channel.send(embed=warning_embed)
-            except Exception as e:
-                # Handle errors during message deletion or sending the warning
-                if log_channel:
-                    await log_channel.send(f"Error handling message deletion or warning: {e}")
    
 async def setup(bot):
     await bot.add_cog(OnMessageLogs(bot))
